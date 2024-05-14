6cc87a1541f5080730d87c893074b885610d5719
AChar1f <acharif41@gmail.com>
2024-05-14 13:55:48 +0200

    day 2 coding exercise

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let firstName  = "Abdurahmaan"
+let lastName = "Charif"
+let fullName = firstName + " " + lastName
+console.log(fullName)
+console.log(typeof fullName)
+const numberVar = 5
+// numberVar * 5 = 25
+const name = "Abdurahmaan"
+name = "Charif"
